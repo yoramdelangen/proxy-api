d4844c201529fc1dcea2355af707995f8162c8fa
Yoram de Langen <yoramdelangen@gmail.nl>
2019-10-18 22:06:16 +0200

    Fixed bug for not selecting podcasts with * statement

--- boot.php
+++ boot.php
@@ -5,6 +5,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 use ClanCats\Hydrahon\Builder;
+use ClanCats\Hydrahon\Query\Expression;
 
 // include this file before every application load.
 $dotenv = Dotenv\Dotenv::create(__DIR__);
@@ -16,12 +17,17 @@ $dotenv->load(true);
 
 function env(string $env)
 {
-	return getenv($env);
+    return getenv($env);
 }
 
 function storage_path(string $path)
 {
     return realpath(realpath(__DIR__.'/./storage/').'/'.$path);
+}
+
+function raw(string $arg): Expression
+{
+    return new Expression($arg);
 }
 
 function connectDb(string $db, string $username, string $password, string $host = 'localhost'): Builder
@@ -32,12 +38,12 @@ function connectDb(string $db, string $username, string $password, string $host 
         $statement = $connection->prepare($queryString);
         $statement->execute($queryParameters);
 
-        if ($query instanceof \ClanCats\Hydrahon\Query\Sql\FetchableInterface) {
-            return $statement->fetchAll(\PDO::FETCH_ASSOC);
-        }
-
         if (filter_var(getenv('DEBUG', false), FILTER_VALIDATE_BOOLEAN)) {
             dump($query, $queryString, $queryParameters);
+        }
+
+        if ($query instanceof \ClanCats\Hydrahon\Query\Sql\FetchableInterface) {
+            return $statement->fetchAll(\PDO::FETCH_ASSOC);
         }
     });
 }
