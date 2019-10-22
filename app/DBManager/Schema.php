ddc1ac9c2ec0b6cf87f2cfd922a7a1cfdc88e81d
Yoram de Langen <yoramdelangen@gmail.nl>
2019-10-22 07:12:42 +0200

    Added getting table schema from MySQL database

--- app/DBManager/Schema.php
+++ app/DBManager/Schema.php
@@ -11,19 +11,36 @@ class Schema
 		$host = 'localhost';
 		$connection = new PDO('mysql:host='.$host, env('DB_PODCAST_USER'), env('DB_PODCAST_PASSWORD'));
 
+		//debug connection
+		// $connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+		$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 		// get a list of all databases.
 		$databases = $connection->query('show databases')->fetchAll(PDO::FETCH_ASSOC);
 		$databases = array_filter(array_map('reset', $databases), function ($db) {
 			return $db !== 'information_schema';
 		});
 
+		$database = 'podcast_db';
+
 		// set database X as database in the connection
-		$connection->query('use bc_topicusplatform');
+		$connection->query('use '. $database);
 
 		// get a list of all tables within the database.
 		$tables = $connection->query('show tables')->fetchAll(PDO::FETCH_ASSOC);
 		$tables = array_map('reset', $tables);
 
-		dd($tables);
+		$table = 'podcasts';
+		// solution 1
+		$schema = $connection->query('DESCRIBE '.$table)->fetchAll(PDO::FETCH_ASSOC);
+
+		return [
+			'database' => $database,
+			'tables' => $tables,
+			'schema' => [
+				'table' => $table,
+				'structure' => $schema,
+			]
+		];
 	}
 }
