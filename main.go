792751665832d53ed82a88ee581d55a6bdd3b759
Yoram <yoramdelangen@gmail.com>
2024-11-22 14:37:53 +0100

    add cors

--- main.go
+++ main.go
@@ -1,19 +1,30 @@
 package main
 
 import (
+	"errors"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/proxy"
 )
 
 func main() {
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:5173, http://localhost:8899",
+		AllowHeaders:     "Content-Type",
+		AllowCredentials: true,
+	}))
 	app.Use(logger.New())
 
 	app.All("/p", func(c *fiber.Ctx) error {
-		url := c.Query("url")
+		url := c.Query("url", "")
+
+		if len(url) == 0 {
+			return errors.New("Invalid request, missing qs")
+		}
 
 		c.Request().URI().QueryArgs().Del("url")
 
@@ -21,6 +32,8 @@ func main() {
 			return err
 		}
 
+		reqs := c.GetReqHeaders()
+		c.Response().Header.Add("Access-Control-Allow-Origin", reqs["Origin"][0])
 		// Remove Server header from response
 		c.Response().Header.Del(fiber.HeaderServer)
 		return nil
